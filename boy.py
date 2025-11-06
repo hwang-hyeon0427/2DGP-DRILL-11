1918de06ed9100dcaf65b92f5238110a73ac1071
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-06 15:59:34 +0900

    튜퓰에 별 추가

--- boy.py
+++ boy.py
@@ -170,6 +170,7 @@ class Boy:
     def draw(self):
         self.state_machine.draw() # 현재 상태
         self.font.draw(self.x-10, self.y + 50, f'{self.ball_count:02d}', (255, 255, 0))
+        draw_rectangle(*self.get_bb())
 
 
     def fire_ball(self):
