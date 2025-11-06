ba3f44ea887a8969d63d7bb675f588230d135238
hwanghyeon(laptop) <jk6054sdov@gmail.com>
2025-11-06 20:15:35 +0900

    변수로 변경

--- zombie.py
+++ zombie.py
@@ -53,9 +53,9 @@ class Zombie:
 
     def draw(self):
         if self.dir < 0:
-            Zombie.images['Walk'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, 200, 200)
+            Zombie.images['Walk'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, self.size , self.size )
         else:
-            Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, 200, 200)
+            Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, self.size , self.size )
         draw_rectangle(*self.get_bb())
 
     def handle_event(self, event):
